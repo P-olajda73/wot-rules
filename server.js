9c0e1deb3b5b795e0d3e5cfaf12667b958a17f64
P-olajda73 <poly2004@seznam.cz>
2026-09-18 10:30:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -36,9 +36,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // 🔹 Routes pre HTML stránky
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
-app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "public", "login.html")));
-app.get("/admin", (req, res) => res.sendFile(path.join(__dirname, "public", "admin.html")));
-
+app.get("/login.html", (req, res) => res.sendFile(path.join(__dirname, "public", "login.html")));
+app.get("/admin.html", (req, res) => res.sendFile(path.join(__dirname, "public", "admin.html")));
 // 🔹 API Endpoints pre pravidlá
 app.get("/rules/:language", async (req, res) => {
     try {
